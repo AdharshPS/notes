562c56537abf155f8ab76f91dd76a87a804fd52b
AdharshPS <adharshps000@gmail.com>
2025-12-18 10:23:56 +0530

    updated theme

--- lib/main.dart
+++ lib/main.dart
@@ -82,6 +82,10 @@ class MainApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Notes App',
+        theme: ThemeData(
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white,
+        ),
         home: const SplashScreen(),
       ),
     );
